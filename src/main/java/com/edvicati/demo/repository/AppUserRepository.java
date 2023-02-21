6ef0fb3d6b162e681c2576518cafeaec4413ae69
edvicaty <edvicaty@hotmail.com>
2023-02-20 20:35:09 -0600

    Add User and Role repositories

---
+++ src/main/java/com/edvicati/demo/repository/AppUserRepository.java
@@ -0,0 +1,9 @@
+package com.edvicati.demo.repository;
+
+import com.edvicati.demo.domain.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+}
