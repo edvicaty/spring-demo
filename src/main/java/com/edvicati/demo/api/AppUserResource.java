374086b68004068418d51ce4c8b8fd561a6c35b2
edvicaty <edvicaty@hotmail.com>
2023-02-21 08:55:35 -0600

    add post methods on User controller

--- src/main/java/com/edvicati/demo/api/AppUserResource.java
+++ src/main/java/com/edvicati/demo/api/AppUserResource.java
@@ -1,13 +1,20 @@
 package com.edvicati.demo.api;
 
 import com.edvicati.demo.domain.AppUser;
+import com.edvicati.demo.domain.Role;
 import com.edvicati.demo.service.AppUserService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Controller
@@ -17,7 +24,34 @@ public class AppUserResource {
     private final AppUserService appUserService;
 
     @GetMapping("/users")
-    ResponseEntity<List<AppUser>> getUsers() {
+    public ResponseEntity<List<AppUser>> getUsers() {
         return ResponseEntity.ok().body(appUserService.getUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<AppUser> saveUser(@RequestBody AppUser user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        // Returning 201 created is a much clearer approach than just returning 200 ok (when creating server data)
+        return ResponseEntity.created(uri).body(appUserService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(appUserService.saveRole(role));
+    }
+
+    // Defining a RoleToUserForm class to access form data
+    @PostMapping("/role/addToUser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form){
+        appUserService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+    @Getter
+    @Setter
+    class RoleToUserForm {
+        private String username;
+        private String roleName;
     }
 }
--- src/main/java/com/edvicati/demo/service/AppUserService.java
+++ src/main/java/com/edvicati/demo/service/AppUserService.java
@@ -6,7 +6,6 @@ import com.edvicati.demo.domain.Role;
 import java.util.List;
 
 // In this case, no RoleService will be created. All the Role logic will be implemented through the AppUser service
-// save user, save role, add role to user by names, get user by username, get all users
 public interface AppUserService {
     AppUser saveUser(AppUser user);
 
@@ -16,7 +15,7 @@ public interface AppUserService {
 
     AppUser getUserByUsername(String username);
 
-    // Is good practice to return a paginated result
+    // TODO: Is good practice to return a paginated result
     List<AppUser> getUsers();
 
 }
